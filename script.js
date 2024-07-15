7bad86c3d810220d72f99b3428ebb77177667bb0
meetmodi45 <meetmmodi45@gmail.com>
2024-07-15 12:32:08 +0530

    made the rocket animation when loading the screen

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-var c= document.querySelector("#cursor")
-document.addEventListener("mousemove",function(dets){
-    c.style.left = dets.x -20+"px"
-    c.style.top = dets.y -20+"px"
-})
+// script.js
+
 document.addEventListener("DOMContentLoaded", function () {
+    // Rocket animation
+    const rocketAnimation = document.getElementById('rocket-animation');
+    const rocket = document.getElementById('rocket');
+    const page1 = document.getElementById('page1');
+
+    rocket.addEventListener('animationend', function () {
+        rocketAnimation.classList.add('hide');
+        document.body.style.overflow = 'auto'; // Re-enable scrolling after the animation
+
+        // Show the homepage after the rocket animation ends and the fade-out transition starts
+        setTimeout(() => {
+            rocketAnimation.style.display = 'none'; // Completely hide after transition
+            page1.style.opacity = '1'; // Fade in the homepage
+        }, 2000); // Duration should match the CSS transition duration
+    });
+    // Text animation
     const changingText = document.querySelector(".changing-text");
     const originalText = "INDUSTRY";
     const newText = "EXPLORATION";
@@ -13,10 +26,10 @@ document.addEventListener("DOMContentLoaded", function () {
         if (currentIndex >= 0) {
             changingText.textContent = originalText.slice(0, currentIndex);
             currentIndex--;
-            setTimeout(backspace, 150); // Adjust the speed of backspacing
+            setTimeout(backspace, 150);
         } else {
             currentIndex = 0;
-            setTimeout(type, 250); // Delay before starting to type
+            setTimeout(type, 250);
         }
     }
 
@@ -24,10 +37,16 @@ document.addEventListener("DOMContentLoaded", function () {
         if (currentIndex <= newText.length) {
             changingText.textContent = newText.slice(0, currentIndex);
             currentIndex++;
-            setTimeout(type, 150); // Adjust the speed of typing
+            setTimeout(type, 150);
         }
     }
 
-    setTimeout(backspace, 1300); // Start backspacing after 2 seconds
+    setTimeout(backspace, 3700);
 });
 
+// Cursor movement
+var c = document.querySelector("#cursor");
+document.addEventListener("mousemove", function (dets) {
+    c.style.left = dets.x - 20 + "px";
+    c.style.top = dets.y - 20 + "px";
+});
